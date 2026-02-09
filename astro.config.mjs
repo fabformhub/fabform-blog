03b0472f1cd1c6992032e5287874487b6c4f5b31
Geoffrey Callaghan <irishgeoff@yahoo.com>
2026-02-09 16:41:18 +0000

    Ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+//
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
+//
 export default defineConfig({
 	site: 'https://blog.fabform.io',
 	integrations: [mdx(), sitemap()],
